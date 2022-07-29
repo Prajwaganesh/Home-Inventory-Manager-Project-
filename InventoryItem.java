47c00402d71ddc0070a18ae7e6e26587ee5d6578
Prajwaganesh <110214451+Prajwaganesh@users.noreply.github.com>
2022-07-29 10:02:58 +0530

    Add files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,16 @@
+package homeinventory;
+public class InventoryItem
+{
+public String description;
+public String location;
+public boolean marked;
+
+public String serialNumber;
+public String purchasePrice;
+public String purchaseDate;
+public String purchaseLocation;
+public String note;
+public String photoFile;
+}
+Footer
+© 2022 GitHub, Inc.
